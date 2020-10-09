e70dfd1bc25d597c350fd10d64220a0c50e6f5ec
rtetelbaum <rtetelbaum@mailbox.org>
2020-10-08 22:24:04 -0400

    -lab done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,17 @@
 class Post < ActiveRecord::Base
 
   belongs_to :author
-  validate :is_title_case 
+	validate :is_title_case 
+	
+	before_validation :make_title_case
+	before_save :email_author_about_post
 
-  private
+	private
+	
+	def email_author_about_post
+    # Not implemented.
+    # For more information: https://guides.rubyonrails.org/action_mailer_basics.html
+  end
 
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
